519937fce7f4b6f811320b221ce88c6c2609c2b0
Yuri Kasperovich <y.kasperovich@dev1team.net>
2014-08-22 11:40:54 +0300

    Altoros task upon interview

--- playground.rb
+++ playground.rb
@@ -1,1 +1,15 @@
 require "./setup"
+require "minitest/autorun"
+
+string = "abcccaba"
+
+def first(str)
+  res = Hash.new(0)
+  
+  str.each_char do |ch|
+    res[ch] += 1
+    return ch if res[ch] > 1
+  end
+end
+
+raise first(string).inspect
